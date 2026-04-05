d10ca3a8ccc7a1061fb7f0e994d127540b573903
gauravgautam2003 <gauravgautam9865@gmail.com>
2026-04-05 14:15:45 +0530

    add useEffect for backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AuthRouter from "./routes/user.routes"
@@ -8,6 +8,17 @@ import IntroAnimation from './components/IntroAnimation'
 
 const App = () => {
     const [introDone, setIntroDone] = useState(false);
+    
+    useEffect(() => {
+        const activateBackend = async () => {
+            try {
+                await fetch(`${import.meta.env.VITE_BACKEND_URL}`);
+            } catch (error) {
+                console.error("Error activating backend:", error);
+            }
+        };
+        activateBackend();
+    }, []);
 
     return (
         <>
